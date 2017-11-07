acbee8087c05ddbb5795af5768dc07a1450b72c7
The-Promised-End <tom.tk.li@gmail.com>
2017-11-07 03:31:16 +0000

    Added token to ignore list

--- download_avatars.js
+++ download_avatars.js
@@ -1,11 +1,21 @@
 var request = require('request');
 
-function getRepoContributors(repoOwner, repoName, cb) {
-
-  getRepoContributors("jquery", "jquery", function(err, result) {
+getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
 });
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      "User-Agent": "result",
+      "Authorization": "token 486af535b36293a4b6ab3d8e8f8d55ac759aa0d0"
+    }
+  };
+  request(options, function(err, res,body) {
+    cb(err, body);
+  });
 
 }
 
