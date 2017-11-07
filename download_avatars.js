19050936cebd001d920cfc1d6fe4e7fd9bfd981e
The-Promised-End <tom.tk.li@gmail.com>
2017-11-07 04:23:25 +0000

    Printing url

--- download_avatars.js
+++ download_avatars.js
@@ -1,23 +1,29 @@
 var request = require('request');
 var secret = require('./secrets.js')
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
-});
 
 function getRepoContributors(repoOwner, repoName, cb) {
+
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       "User-Agent": "request",
       "Authorization": "token " + secret.GITHUB_TOKEN
-    }
+    },
+    json : true
   };
+
   request(options, function(err, res,body) {
     cb(err, body);
   });
-
 }
+getRepoContributors("jquery", "jquery", function(err, contributors) {
+  console.log("Errors:", err);
+  contributors.forEach(function(contributor) {
+    console.log("Result:", contributor.avatar_url);
+  })
+
+});
+
 
 console.log("Welcome to the GitHub Avatar Downloader!");
